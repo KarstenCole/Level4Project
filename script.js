330ef1c15a69c509953bff4cbd136719aad73f0e
Karsten <99371148+KarstenCole@users.noreply.github.com>
2023-01-05 23:30:02 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+function initialize(){
+
+    box1 = document.getElementById("one");
+    box2 = document.getElementById("two");
+    box3 = document.getElementById("three");
+    box4 = document.getElementById("four");
+    box5 = document.getElementById("five");
+    box6 = document.getElementById("six");
+    box7 = document.getElementById("seven");
+    box8 = document.getElementById("eight");
+    box9 = document.getElementById("nine");
+
+    boxes = [box1, box2, box3,
+             box4, box5, box6,
+             box7, box8, box9];
+    
+    display();
+}
+
+function display(){
+    
+}

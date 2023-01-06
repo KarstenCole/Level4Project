28f75ce9f8c86baaa018364a8e22f5e2fa862097
Karsten <99371148+KarstenCole@users.noreply.github.com>
2023-01-06 14:02:24 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 function initialize(){
-
+    CurrentPlayer = null;
+    x_o = ""; 
     box1 = document.getElementById("one");
     box2 = document.getElementById("two");
     box3 = document.getElementById("three");
@@ -9,14 +10,49 @@ function initialize(){
     box7 = document.getElementById("seven");
     box8 = document.getElementById("eight");
     box9 = document.getElementById("nine");
+    boxD1 = "";  boxD2 = ""; boxD3 = "";  boxD4 = "";  boxD5 = "";  boxD6 = "";  boxD7 = "";   boxD8 = "";  boxD9 = ""; 
 
-    boxes = [box1, box2, box3,
-             box4, box5, box6,
-             box7, box8, box9];
+    boxes = [box1, box2, box3, box4, box5, box6, box7, box8, box9];
+    boxDisplays = [ boxD1, boxD2, boxD3, boxD4, boxD5, boxD6, boxD7, boxD8, boxD9];
+    twoLevelChoices = ["","","","","","","","",""];
     
     display();
 }
 
-function display(){
+function XorO(XorO){
+    if(x_o === ""){
+        x_o = XorO;
+        if(x_o === 'X') {
+            CurrentPlayer = "player";
+        }
+        else{
+            CurrentPlayer = "computer";
+            computerTurn();
+        } 
+    }
+}
+
+function choose(button){
+    if(CurrentPlayer === "player"){
+        boxDisplays[button-1] = x_o;
     
+        // switches player
+        CurrentPlayer = CurrentPlayer === "player" ? "computer" : "player";
+        if(CurrentPlayer === "computer"){
+            computerTurn();
+        }
+
+
+        display();
+    }
+}
+
+function computerTurn(){
+
+}
+
+function display(){
+    for(var i=0; i<boxes.length; i++){
+        boxes[i].innerHTML = boxDisplays[i];
+    }
 }
